f72b898265e6253af2e4d020666ad9e1eb3dbef5
Sarah <sarahboureghda@yahoo.com>
2026-09-09 14:02:50 -0400

    Update data_prep.R

--- data_prep.R
+++ data_prep.R
@@ -1,4 +1,9 @@
-install.packages("tidyverse")
+
+list.of.packages <- c("tidyverse", "arcgislayers", "arcgisutils", "sf")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+
 library(tidyverse)
 library(arcgislayers)
 library(arcgisutils)

3ddd80954def754fbb7127a9ba66588ececd821d
Sarah <sarahboureghda@yahoo.com>
2026-09-09 14:03:17 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,3 +1,7 @@
+list.of.packages <- c("shiny", "DT", "ggplot2")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(shiny)
 library(DT)
 library(ggplot2)
